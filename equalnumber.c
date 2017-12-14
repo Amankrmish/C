9ac7d9b8c62468a9783630b137fb534b17c26900
Amankrmish <32844580+Amankrmish@users.noreply.github.com>
2017-12-14 20:46:47 +0530

    Create equalnumber.c

---
+++ equalnumber.c
@@ -0,0 +1,17 @@
+/*check two number are equal are not*/
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter two number");
+	scanf("%d%d",&a,&b);
+	if(a==b)
+	{
+		printf("Number are equal");
+	}
+	else
+	
+		printf("Number are not equal");
+	
+	return 0;
+}
